12c85fa7d62ede1898cc1704a155e233f9a2d35f
Orange Label <andyduke@orangelabel.ru>
2016-06-01 13:57:39 +0300

    Added support for results paging

--- Basecamp/Api/AbstractApi.php
+++ Basecamp/Api/AbstractApi.php
@@ -11,6 +11,7 @@ abstract class AbstractApi
 {
     protected $client = null;
     private $timeout = 10;
+    protected $page_size = 50;
 
     public function __construct(Client $client)
     {
@@ -20,8 +21,21 @@ abstract class AbstractApi
     final protected function get($resource, array $params = [])
     {
         $resource = empty($params) ? $resource : ($resource . (false === strpos($resource, '?') ? '?' : '&') . http_build_query($params, '', '&'));
-
         $response = $this->client->request(Request::METHOD_GET, $resource, [], $this->timeout);
+
+        // Fetch next pages
+        $page = 2;
+        $response_count = count($response);
+        while ($response_count == $this->page_size) {
+          $next_params = empty($params) ? ['page' => $page] : array_merge($params, ['page' => $page]);
+          $next_resource = ($resource . (false === strpos($resource, '?') ? '?' : '&') . http_build_query($next_params, '', '&'));
+          $next_response = $this->client->request(Request::METHOD_GET, $next_resource, [], $this->timeout);
+          if (!empty($next_response)) {
+            $response = array_merge($response, $next_response);
+          }
+          $response_count = count($next_response);
+          $page++;
+        }
 
         return $response;
     }
